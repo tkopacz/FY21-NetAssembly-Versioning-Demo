655219249e24753e577a020449dbd37791f38a01
Tomasz Kopacz <tkopacz@tomaszkopacz.com>
2021-08-22 18:36:11 +0200

    Work

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace demo
 {
@@ -6,7 +7,42 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myAsm = typeof(Program).Assembly;
+
+            Console.WriteLine("Begin");
+            //Display(0,$"{myAsm}")
+            //System.Console.WriteLine(myAsm.FullName, myAsm.Version);
+            myAsm.CustomAttributes;
+            DisplayAttributes(.GetCustomAttribute());
+            Console.WriteLine("End");
+        }
+
+        public static void DisplayAttributes(Int32 indent, MemberInfo mi)
+        {
+            // Get the set of custom attributes; if none exist, just return.
+            object[] attrs = mi.GetCustomAttributes(false);
+            if (attrs.Length == 0) { return; }
+            DisplayAttributes(indent,attrs);
+        }
+
+        public static void DisplayAttributes(Int32 indent, object[] attrs)
+        {
+            // Get the set of custom attributes; if none exist, just return.
+            object[] attrs = mi.GetCustomAttributes(false);
+
+            // Display the custom attributes applied to this member.
+            Display(indent + 1, "Attributes:");
+            foreach (object o in attrs)
+            {
+                Display(indent + 2, "{0}", o.ToString());
+            }
+        }
+        // Display a formatted string indented by the specified amount.
+        public static void Display(Int32 indent, string format, params object[] param)
+
+        {
+            Console.Write(new string(' ', indent * 2));
+            Console.WriteLine(format, param);
         }
     }
 }
